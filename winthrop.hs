7a9ac81fdcc15cebc0c2c9b09e8ab725786ed08b
JDulin <dulinjd@gmail.com>
2013-02-23 04:28:36 -0500

    Worked on parser

--- winthrop.hs
+++ winthrop.hs
@@ -1,11 +1,13 @@
 -- John Dulin
--- January 27, 2012
+-- John Dulin
+-- February 2013
 -- Hi, my name ith Winthrop!
 -- I'm a thimple lithp proceththor!
 
 module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
+import Monad
 
 main :: IO ()
 main = do args <- getArgs
@@ -21,10 +23,32 @@ data LispVal = Atom String
 symbol :: Parser Char
 symbol = oneOf "!$+-*/^_&|%<=?>#~:@"
 
+parseAtom :: Parser LispVal
+parseAtom = do first <- symbol <|> letter
+	       rest <- symbol <|> number <|> letter
+	       return $ case atom of
+               		"#t" -> Bool True
+			"#f" -> Bool False
+			otherwise -> Atom atom
+
+
+parseString :: Parser LispVal
+parseString = do char '"'
+		 x <- many (noneOf "\""
+		 char '"'
+		 return $ String x
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
 spaces :: Parser ()
 spaces = skipMany1 space
 
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+	    <|> parseString
+
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
 	Left err -> "No match " ++ show err
 	Right err -> "Found Value"
